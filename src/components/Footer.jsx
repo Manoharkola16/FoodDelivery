28b5fc73967a9ce83500e2f0832ba6993afb8b55
Manoharkola16 <manoharkola16@gmail.com>
2026-05-14 16:52:42 +0530

    updated asset image names

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="text-center py-6 bg-gray-800 text-white">
-      <p>© 2026 MyKitchen.  Enojoy Your Food.</p>
-    </footer>
-  );
-};
-
-export default Footer;
